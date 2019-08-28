1309004d6b0a85b3eab6be63d7077c9122c9597d
firas-ammar <firas.ammar.ing@gmail.com>
2019-08-28 18:00:59 +0200

    Updates

---
+++ precache-manifest.ddbb09adc05a688a3a3f3e5aa3fd6175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20343152dc45d81884d7099841d1642",
+    "url": "/Cards-input/index.html"
+  },
+  {
+    "revision": "141472d3ebdd0b465a4c",
+    "url": "/Cards-input/static/css/main.fb73b75a.chunk.css"
+  },
+  {
+    "revision": "1933fb0bc3a45288556c",
+    "url": "/Cards-input/static/js/2.dfafeda7.chunk.js"
+  },
+  {
+    "revision": "141472d3ebdd0b465a4c",
+    "url": "/Cards-input/static/js/main.4f02e50a.chunk.js"
+  },
+  {
+    "revision": "4f038f0ad847e0191b24",
+    "url": "/Cards-input/static/js/runtime~main.e91d9be5.js"
+  },
+  {
+    "revision": "7041a77792b37b9b8ee59dc77d8abc24",
+    "url": "/Cards-input/static/media/puce.7041a777.png"
+  },
+  {
+    "revision": "7ffd65fec581abf5ef288ed247877d5b",
+    "url": "/Cards-input/static/media/visa.7ffd65fe.png"
+  }
+]);
